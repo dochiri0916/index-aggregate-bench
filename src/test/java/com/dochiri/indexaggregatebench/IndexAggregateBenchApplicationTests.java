2ba9dad4db79c1224f9b8dbf0f066ada640a969f
송성빈 <dochiri0916@gmail.com>
2026-05-04 21:00:23 +0900

    test: 애플리케이션 컨텍스트 테스트 추가

---
+++ src/test/java/com/dochiri/indexaggregatebench/IndexAggregateBenchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dochiri.indexaggregatebench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexAggregateBenchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
